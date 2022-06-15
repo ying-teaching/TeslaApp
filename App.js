64a17469e0790c17a7a90b20f92c2c2575164c28
Ying Liu <yingliu4203@gmail.com>
2022-06-14 21:43:04 -0700

    add header

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
+import Header from './components/Header';
 import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarsList />
       <StatusBar style="auto" />
     </View>
---
+++ components/Header/index.jsx
@@ -0,0 +1,38 @@
+import { View, Image, StyleSheet } from 'react-native';
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/images/logo.png')}
+      />
+      <Image
+        style={styles.menu}
+        source={require('../../assets/images/menu.png')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 50,
+    zIndex: 100,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 100,
+    height: 20,
+    resizeMode: 'contain',
+  },
+  menu: {
+    width: 25,
+    height: 25,
+    resizeMode: 'contain',
+  },
+});
